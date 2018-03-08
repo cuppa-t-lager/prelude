25239020d8b2721affad3c83797c1e57d7fe36ad
John Nicola <john.nicola@concreteplatform.com>
2018-03-08 21:33:55 +0000

    addd missing files

---
+++ elpa/find-file-in-repository-20151113.519/find-file-in-repository-autoloads.el
@@ -0,0 +1,37 @@
+;;; find-file-in-repository-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "find-file-in-repository" "find-file-in-repository.el"
+;;;;;;  (23032 38815 0 0))
+;;; Generated autoloads from find-file-in-repository.el
+
+(autoload 'find-file-in-repository "find-file-in-repository" "\
+find-file-in-repository will autocomplete all files in the
+   current git, mercurial or other type of repository, using
+   ido-find-file when available. When the current file is not
+   located inside of any repository, falls back on a regular
+   find-file operation.
+
+\(fn)" t nil)
+
+(defalias 'ffir 'find-file-in-repository)
+
+(put 'ffir-repository-types 'safe-local-variable 'listp)
+
+(put 'ffir-avoid-HOME-repository 'safe-local-variable 'booleanp)
+
+;;;***
+
+;;;### (autoloads nil nil ("find-file-in-repository-pkg.el") (23032
+;;;;;;  38807 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; find-file-in-repository-autoloads.el ends here
---
+++ personal/custom.el
@@ -0,0 +1,87 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ace-isearch-func-delay 1)
+ '(ace-isearch-jump-delay 1.3)
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(auto-dim-other-buffers-mode t t)
+ '(back-button-global-backward-keystrokes (quote ("C-x <C-left>")))
+ '(back-button-global-forward-keystrokes (quote ("C-x <C-right>")))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (material)))
+ '(custom-safe-themes
+   (quote
+	("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" default)))
+ '(ediff-diff-options "-w")
+ '(ediff-highlight-all-diffs nil)
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(expand-region-smart-cursor t)
+ '(fringe-mode 10 nil (fringe))
+ '(global-diff-hl-mode t)
+ '(helm-rg-ignore-case t)
+ '(helm-rg-smart-case t)
+ '(hl-sexp-background-color "#1c1f26")
+ '(js-indent-level 0)
+ '(js3-auto-indent-p nil)
+ '(js3-boring-indentation t)
+ '(js3-consistent-level-indent-inner-bracket t)
+ '(js3-continued-expr-mult 4)
+ '(js3-curly-indent-offset 0)
+ '(js3-enter-indents-newline t)
+ '(js3-expr-indent-offset 4)
+ '(js3-indent-dots t)
+ '(js3-indent-leve 4)
+ '(js3-indent-level 4)
+ '(js3-indent-on-enter-key t)
+ '(js3-indent-tabs-mode t)
+ '(js3-lazy-commas t)
+ '(js3-lazy-dots t)
+ '(js3-lazy-operators t)
+ '(js3-mode-show-parse-errors nil)
+ '(js3-mode-show-strict-warnings nil)
+ '(js3-paren-indent-offset 0)
+ '(js3-square-indent-offset 0)
+ '(js3-strict-cond-assign-warning nil)
+ '(js3-strict-inconsistent-return-warning nil)
+ '(js3-strict-trailing-comma-warning nil)
+ '(js3-strict-var-hides-function-arg-warning nil)
+ '(js3-strict-var-redeclaration-warning nil)
+ '(mac-command-modifier (quote super))
+ '(mac-control-modifier (quote control))
+ '(mac-function-modifier (quote hyper))
+ '(mac-option-modifier (quote meta))
+ '(mac-pass-command-to-system nil)
+ '(magit-diff-hide-trailing-cr-characters t)
+ '(magit-diff-section-arguments (quote ("--ignore-space-change" "--no-ext-diff")))
+ '(mweb-default-major-mode (quote javascript-mode))
+ '(nrepl-message-colors
+   (quote
+	("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+	(sql-indent string-inflection swiper helm-c-yasnippet js2-mode dockerfile-mode json-mode company helm-company poly back-button use-package projectile-speedbar helm-dash find-file-in-project find-file-in-repository dumb-jump material-theme hc-zenburn-theme js3-mode emmet-mode powerline sr-speedbar idle-highlight-mode ac-helm ace-isearch magit-gitflow iy-go-to-char helm-ag helm-descbinds auto-yasnippet multiple-cursors helm-projectile key-chord helm rainbow-delimiters vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(prelude-whitespace nil)
+ '(projectile-switch-project-action (quote helm-projectile-recentf))
+ '(setq js3-indent-tabs-mode)
+ '(tab-stop-list
+   (quote
+	(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-dim-other-buffers-face ((t (:background "gray20"))))
+ '(diff-added ((t (:foreground "#8bc34a"))))
+ '(diff-removed ((t (:foreground "brown"))))
+ '(helm-selection ((t (:background "DeepSkyBlue4"))))
+ '(js3-error-face ((t nil)))
+ '(js3-magic-paren-face ((t nil)))
+ '(js3-warning-face ((t nil))))
